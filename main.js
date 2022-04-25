38d1eca6a5ba3b5da2808a82f86bad86f5a769b4
Gregori Franco <gregoricardosofranco@gmail.com>
2022-04-25 20:34:24 -0300

    correcao de bugs

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-var tbClientes = localStorage.getItem("tbClientes");
-tbClientes = JSON.parse(tbClientes);
-if (tbClientes == null) {
-    const tbClientes = [{}];
-    const myObj = { titulo: "teste", descricao: "descricao teste", etapa: "aFazer", data: "" };
-    tbClientes.push(myObj);
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+var tbTarefas = localStorage.getItem("tbTarefas");
+tbTarefas = JSON.parse(tbTarefas);
+if (tbTarefas == null) {
+    tbTarefas = [];
+    localStorage.setItem("tbTarefas", JSON.stringify(tbTarefas));
 } else {
-    montar(tbClientes);
+    montar(tbTarefas);
 }
 
 
-function montar(tbClientes) {
-    console.log(tbClientes);
+function montar(tbTarefas) {
+    console.log(tbTarefas);
     var cardCor = "";
-    tbClientes.forEach(function (tarefas, i) {
+    tbTarefas.forEach(function (tarefas, i) {
         console.log("case : " + tarefas.etapa);
         var divEtapa = "#" + tarefas.etapa
         switch (tarefas.etapa) {
@@ -37,9 +35,9 @@ function montar(tbClientes) {
             <div class="card-header `+ cardCor + ` "></div>
                 <div class="card-body">
                 <h5 class="card-title">`+ primeiraLetraMaiscula(tarefas.titulo) + `</h5>
-                <p class="card-text">`+  primeiraLetraMaiscula(tarefas.descricao) + `</p>
+                <p class="card-text">`+ primeiraLetraMaiscula(tarefas.descricao) + `</p>
                 <a href="#" class="btn btn-danger"  onclick="deletar(`+ i + `)">Deletar</a>
-                <a href="#" class="btn btn-warning"  onclick="editar(`+ i + `)"  data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</a>
+               
                 </div>
                 <div class="card-footer bg-transparent border-success"> <small class="text-muted"> Criado: `+ tarefas.data + `</small></div>
         </div>
@@ -48,48 +46,33 @@ function montar(tbClientes) {
     })
 }
 function primeiraLetraMaiscula(palavra) {
-    if(palavra){
+    if (palavra) {
         return palavra.charAt(0).toUpperCase() + palavra.slice(1);
     }
 }
 
-function editar(posicao) {
-    var objectLocal = localStorage.getItem('tbClientes');
-    objectLocal = JSON.parse(objectLocal);
-    var txtTitulo = $("#titulo").val(objectLocal[posicao].titulo);
-    var txtEtapa = $("#etapa").val(objectLocal[posicao].etapa);
-    var txtDescricao = $("#descricao").val(objectLocal[posicao].descricao);
-
-    tbClientes[posicao] = JSON.parse(JSON.stringify(
-        {
-            titulo: $("#titulo").val(),
-            data: objectLocal[posicao].data,
-            descricao: $("#descricao").val(),
-            etapa: $("#etapa").val(),
-        }
-    ));
-    toast("Editado")
-}
-
 function deletar(i) {
-    tbClientes.splice(i, 1);
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+    tbTarefas.splice(i, 1);
+    localStorage.setItem("tbTarefas", JSON.stringify(tbTarefas));
     console.log(i);
     $("#" + i).hide();
     toast("Deletado")
 }
+
 function salvar() {
     const data = new Date();
-    let dataFormatada = (data.getDate() + "/" + ((data.getMonth() + 1)) + "/" + (data.getFullYear()));
-    var tbClientes = localStorage.getItem("tbClientes");
-    let obj = JSON.parse(tbClientes);
+    var dataFormatada = (data.getDate() + "/" + ((data.getMonth() + 1)) + "/" + (data.getFullYear()));
+
+    var tbTarefas = localStorage.getItem("tbTarefas");
+    var obj = JSON.parse(tbTarefas);
     var txtTitulo = $("#titulo").val();
     var txtEtapa = $("#etapa").val();
     var txtDescricao = $("#descricao").val();
     const myObj = { titulo: txtTitulo, descricao: txtDescricao, etapa: txtEtapa, data: dataFormatada };
     obj.push(myObj);
-    localStorage.setItem("tbClientes", JSON.stringify(obj));
+    localStorage.setItem("tbTarefas", JSON.stringify(obj));
     window.location.reload();
+
 }
 
 const cards = document.querySelectorAll(".card");
@@ -160,9 +143,9 @@ function dragover() {
     let posicao = parseInt(cardSelect.id);
     montaCores(this.id, cardSelectHeader);
     this.appendChild(cardSelect)
-    var objectLocal = localStorage.getItem('tbClientes');
+    var objectLocal = localStorage.getItem('tbTarefas');
     objectLocal = JSON.parse(objectLocal);
-    tbClientes[posicao] = JSON.parse(JSON.stringify(
+    tbTarefas[posicao] = JSON.parse(JSON.stringify(
         {
             titulo: objectLocal[posicao].titulo,
             data: objectLocal[posicao].data,
@@ -171,7 +154,7 @@ function dragover() {
         }
     ));
 
-    localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
+    localStorage.setItem("tbTarefas", JSON.stringify(tbTarefas));
     toast("Tarefa :" + objectLocal[posicao].titulo + ", Etapa :" + this.id + " !")
 }
 
